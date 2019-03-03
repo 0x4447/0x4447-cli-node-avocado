6aa3b9213c744f9d0fc44787b8025d5506380f52
jarmahent <kjh7796@gmail.com>
2019-03-03 17:30:36 -0500

    tests for travis closes isue #36

--- test/default.js
+++ test/default.js
@@ -1,8 +1,23 @@
+const exec = require("child_process").exec;
+const npm = require("npm");
+const process = require("process");
+const chai = require("chai");
+
 describe('For TravisCI', function() {
 
-	it('It work!', function() {
+	it('Build test site to make Avocado works.', function(done) {
 
-		return;
+		exec('cd ./test/sample_site && node ../../index.js -s .', function(error, stdout, stderr) {
+			if (error !== null) {
+		        console.log('exec error: ' + error);
+				process.exit(-1);
+		    }
+			chai.assert(null, error);
+
+		    console.log('stdout: ' + stdout);
+		    console.log('stderr: ' + stderr);
+			done()
+		});
 
 	});
 
