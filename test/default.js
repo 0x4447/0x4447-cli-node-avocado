fcfbcd5b632425ecf8439497fa56319f16fd729c
David Gatti <github@gatti.email>
2019-03-09 11:20:23 +0100

    Cleanup

--- test/default.js
+++ test/default.js
@@ -1,37 +1,34 @@
-const exec = require("child_process").exec;
-const chai = require("chai");
+let exec = require("child_process").exec;
+let chai = require("chai");
 
 describe('For TravisCI', function() {
 
 	it('Build test site to make sure this version works.', function(done) {
 
 		//
-		// Execute index.js on the sample site, if it builds it means
-		// this version works.
+		//	Execute index.js on the sample site, if it builds it means
+		//	this version works.
 		//
-
 		exec('cd ./test/sample_site && node ../../index.js -s .',
 		function(error, stdout, stderr) {
 
 			//
-			// If error is not null it means the version has failed
+			//	If error is not null it means the version has failed
 			//
-
-			if (error !== null) {
-		        console.log('exec error: ' + error);
-		    }
+			if(error !== null)
+			{
+				console.log('exec error: ' + error);
+			}
 
 			//
-			// Force the test to fail on Travis
+			//	Force the test to fail on Travis
 			//
-
 			chai.assert.equal(null, error);
 
-		    //
-			// If the tests dont fail then done() will execute and pass
 			//
-
-			done()
+			//	If the tests dont fail then done() will execute and pass
+			//
+			done();
 
 		});
 
