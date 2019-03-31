c9ea3d66547b064fd2044bd2866a81e045fa926a
David Gatti <github@gatti.email>
2019-03-31 11:46:12 +0200

    Cleanup

--- test/default.js
+++ test/default.js
@@ -1,33 +1,24 @@
 let exec = require("child_process").exec;
 let chai = require("chai");
 
-describe('For TravisCI', function() {
+describe('Build', function() {
 
-	it('Build test site to make sure this version works.', function(done) {
+	it('Use empty page.', function(done) {
 
 		//
 		//	Execute index.js on the sample site, if it builds it means
 		//	this version works.
 		//
-
 		exec('cd ./test/dummy_site && node ../../index.js -s . && rm -rf _output && rm -rf _preview',
 		function(error, stdout, stderr) {
 
 			//
-			//	If error is not null it means the version has failed
-			//
-			if(error !== null)
-			{
-				console.log('exec error: ' + error);
-			}
-
-			//
-			//	Force the test to fail on Travis
+			//	1.	Check if we got an error from the CLI.
 			//
 			chai.assert.equal(null, error);
 
 			//
-			//	If the tests dont fail then done() will execute and pass
+			//	-> Tell chai that we are done with our test.
 			//
 			done();
 
